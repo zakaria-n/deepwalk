96186c191c59500bcfe42262b5111a528114c053
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2022-01-08 13:48:42 +0100

    added data

--- job.sh
+++ job.sh
@@ -1,2 +1,2 @@
-python3 deepwalk.py --input /Users/zak/Documents/GitHub/Network-Representation-Learning/data/cora/cora.edgelist --output embeddings/cora.emb --undirected False
-python3 deepwalk.py --input /Users/zak/Documents/GitHub/Network-Representation-Learning/data/epinion/epinion.edgelist --output embeddings/epinion.emb --undirected False
+python3 deepwalk.py --input data/cora.edgelist --output embeddings/cora.emb --undirected False
+python3 deepwalk.py --input data/epinion.edgelist --output embeddings/epinion.emb --undirected False
